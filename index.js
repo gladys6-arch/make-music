a03d065914fc706fe551971c4346b1af93161753
gladys <achandogladys6@gmail.com>
2025-06-26 14:51:45 +0300

    add new changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Audio =document.getElementById('audio');
 const playbutton=document.getElementById('play-btn');
 
 
-let song = [];
+let songs = [];
 let currentIndex =0;
 
 searchBtn.addEventListener('click',()=>{
@@ -16,4 +16,27 @@ searchBtn.addEventListener('click',()=>{
     searchMusic(query);
   }
 });
+
+function searchMusic(query) {
+    const url = `https://itunes.apple.com/search?term=${encodeURIComponent(query)}&entity=song&limit=10`;
+
+    fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+      .then(res => res.json())
+      .then(data => {
+        songs = data.results;
+        displayResults(songs);
+      })
+      .catch(console.error);
+  }
+
+  function displayResults(results){
+    resultsList.innerHTML="";
+
+    results.forEach((songs,index)=>{
+      const li =document.createElement('li');
+      li.innerHTML=
+    })
+  }
+
+
 
