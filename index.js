69df5b66173f12eb24d5fb44bfcc8a11084f973f
gladys <achandogladys6@gmail.com>
2025-06-26 14:36:24 +0300

    add new changes in index.js

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const resultsList = document.getElementById('results');
 const currentsong=document.getElementById('current-song-title');
 const Audio =document.getElementById('audio');
 const playbutton=document.getElementById('play-btn');
+
+
+let song = [];
+let currentIndex =0;
+
+searchBtn.addEventListener('click',()=>{
+  const query = searchInput.ariaValueMax.trim();
+  if(query){
+    searchMusic(query);
+  }
+});
+
