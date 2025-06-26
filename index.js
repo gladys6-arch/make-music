51993703d7c81d12507bf38430040953ab0942f4
gladys <achandogladys6@gmail.com>
2025-06-26 14:25:36 +0300

    add new changes in index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// get element by id
+const searchInput=document.getElementById('search-input');
+const searchBtn=document.getElementById('search-btn');
+const resultsList = document.getElementById('results');
+const currentsong=document.getElementById('current-song-title');
+const Audio =document.getElementById('audio');
+const playbutton=document.getElementById('play-btn');
